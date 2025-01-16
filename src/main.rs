be9f3511765c7864e42e214c55c95455a796eb3f
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2025-01-16 03:01:27 -0700

    flash red and blue; don't print

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use ch32_hal as hal;
 use core::iter::once;
 use hal::delay::Delay;
 use hal::gpio::{Level, Output, Speed};
-use hal::println;
 use smart_leds::{SmartLedsWrite, RGB8};
 use ws2812_delay::Ws2812;
 
@@ -22,22 +21,24 @@ fn main() -> ! {
     let pin = Output::new(p.PA4, Level::Low, Speed::High);
     let mut led = Ws2812::new(Delay, pin);
 
-    let color = RGB8 {
-        r: 255,
-        g: 0,
-        b: 255,
-    };
+    let mut color = RGB8 { r: 255, g: 0, b: 0 };
 
     let _ = led.write(once(color));
 
-    delay.delay_ms(500);
-    
-    loop {}
+    loop {
+        if color.r == 255 {
+            color = RGB8 { r: 0, g: 0, b: 255 }
+        } else {
+            color = RGB8 { r: 255, g: 0, b: 0 }
+        };
+        
+        let _ = led.write(once(color));
+
+        delay.delay_ms(500);
+    }
 }
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    let _ = println!("\n\n\n{}", info);
-
     loop {}
 }
