6582b77f3340347dbb10f32c9d8e6ffd360eed2e
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2025-01-16 03:01:27 -0700

    write solid pink to the neopixel

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,41 @@
 #![feature(type_alias_impl_trait)]
 #![feature(impl_trait_in_assoc_type)]
 
-use hal::gpio::{Level, Output};
-use qingke::riscv;
-use {ch32_hal as hal, panic_halt as _};
+use ch32_hal as hal;
+use core::iter::once;
+use hal::delay::Delay;
+use hal::gpio::{Level, Output, Speed};
+use hal::println;
+use smart_leds::{SmartLedsWrite, RGB8};
+use ws2812_delay::Ws2812;
 
 #[qingke_rt::entry]
 fn main() -> ! {
-    let p = hal::init(Default::default());
+    let mut config = hal::Config::default();
+    config.rcc = hal::rcc::Config::SYSCLK_FREQ_144MHZ_HSI;
+    let p = hal::init(config);
 
-    let mut led = Output::new(p.PA15, Level::Low, Default::default());
-    loop {
-        led.toggle();
+    let mut delay = Delay;
 
-        riscv::asm::delay(1000000);
-    }
+    let pin = Output::new(p.PA4, Level::Low, Speed::High);
+    let mut led = Ws2812::new(Delay, pin);
+
+    let color = RGB8 {
+        r: 255,
+        g: 0,
+        b: 255,
+    };
+
+    let _ = led.write(once(color));
+
+    delay.delay_ms(500);
+    
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    let _ = println!("\n\n\n{}", info);
+
+    loop {}
 }
